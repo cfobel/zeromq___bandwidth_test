91f52d6010eb08d1b8f295149d30aa8bc95575fd
Christian Fobel <christian@fobel.net>
2012-10-24 14:10:05 -0400

    Add functions to test bind side and connect side
    If `bind` is specified, the process will loop forever, accepting
    connections from any connecting `connect` clients.

--- zmq_bandwidth_test.py
+++ zmq_bandwidth_test.py
@@ -23,6 +23,50 @@ def kbytes_per_second(start, end, data):
     return int(data.size * data.dtype.itemsize / total_seconds(end - start)) >> 10
 
 
+
+def test_rep(port):
+    sock = zmq.Socket(ctx, zmq.REP)
+    sock.bind('tcp://*:54321')
+
+    while True:
+        message = sock.recv_pyobj()
+        if message == 'READY':
+            sock.send_pyobj('OK')
+            end, start, data = timed_recv(sock)
+            sock.send_pyobj('DONE')
+
+            message = sock.recv_pyobj()
+            if message == 'OK':
+                # Other end has started the blocking read, so send the data payload.
+                sock.send_pyobj(data)
+                message = sock.recv_pyobj()
+                if message == 'DONE':
+                    sock.send_pyobj('OK')
+
+
+def test_req(port, byte_count):
+    sock = zmq.Socket(ctx, zmq.REQ)
+    sock.connect('tcp://localhost:54321')
+    # Inform the other end that we are ready to send some data.  The other end
+    # responds with 'OK' to let us know when it is about to start waiting on a
+    # blocking receive of the data payload.
+    sock.send_pyobj('READY')
+    message = sock.recv_pyobj()
+    if message == 'OK':
+        # Other end has started the blocking read, so send the data payload.
+        send_bytes(sock, byte_count)
+        message = sock.recv_pyobj()
+
+        if message == 'DONE':
+            sock.send_pyobj('OK')
+            end, start, data = timed_recv(sock)
+            sock.send_pyobj('DONE')
+            message = sock.recv_pyobj()
+            if message == 'OK':
+                print '[OK] Test complete for port={}, payload size (kB)={}'\
+                        .format(port, byte_count)
+
+
 if __name__ == '__main__':
     import sys
 
@@ -31,12 +75,11 @@ if __name__ == '__main__':
 
     if not len(sys.argv) == 2 or sys.argv[1] not in ('bind', 'connect'):
         print_usage()
-    
+
     ctx = zmq.Context.instance()
 
+    port = 54321
     if sys.argv[1] == 'bind':
-        sock = zmq.Socket(ctx, zmq.REP)
-        sock.bind('tcp://*:54321')
+        test_rep(port)
     else:
-        sock = zmq.Socket(ctx, zmq.REQ)
-        sock.connect('tcp://remote.fobel.net:54321')
+        test_req(port, 1 << 20)
